2589228a0f4c95bce5316fc8e40b1b8827828acd
Matias Borghi <borghi.matias@gmail.com>
2024-07-24 15:30:06 -0300

    Set up a simple form to submit client issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
 import Link from "next/link";
 
 export default function Page() {
+  async function submitForm(formData: FormData) {
+    "use server";
+    const rawFormData = Object.fromEntries(formData);
+    console.log({ rawFormData });
+  }
+
   return (
     <div>
       <h1>Home</h1>
       <Link href="/about">About</Link>
+      <form action={submitForm}>
+        <div>
+          <label htmlFor="issue-description">Issue description:</label>
+          <textarea name="issue-description"></textarea>
+        </div>
+
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" id="name"></input>
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="text" name="email" id="email"></input>
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
